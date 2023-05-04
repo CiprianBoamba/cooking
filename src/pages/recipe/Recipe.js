90aa2bfeef97f629bd76d45c5c71a240fbc3c3a6
CiprianBoamba <ciprian.boamba@gmail.com>
2023-05-04 17:20:54 +0100

    add

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -10,12 +10,14 @@ import { useFetch } from '../../hooks/useFetch ';
 export default function Recipe() {
   const { id } = useParams();
 
-  const url = ' http://localhost:3000/recipes' + id;
+  const url = ' http://localhost:3000/recipes/' + id;
 
-  const { data } = useFetch(url);
+  const { error, isPending, data: recipe } = useFetch(url);
   return (
     <div>
-      <h2>{data.title}</h2>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipe && <h1>{recipe.title}</h1>}
     </div>
   );
 }
