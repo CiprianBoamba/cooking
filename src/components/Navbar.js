9b0cd26cdb7b8133f1b6c0eda318c41cdf54f314
CiprianBoamba <ciprian.boamba@gmail.com>
2023-05-09 07:16:22 +0100

    add context-provider

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Searchbar from './Searchbar';
+import { useContext } from 'react';
+import { ThemeContext } from '../context/ThemeContext';
 
 // style
 import './Navbar.css';
 
 export default function Navbar() {
+  const { color } = useContext(ThemeContext);
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ backgroundColor: color }}>
       <nav>
         <Link to="/" className="brand">
           <h1>Cooking</h1>
